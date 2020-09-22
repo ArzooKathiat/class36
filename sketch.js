c711f9f0ef33bce02243bd23f71d1e20d3ef483e
ArzooKathiat <69519775+ArzooKathiat@users.noreply.github.com>
2020-09-22 20:09:37 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,34 @@
+var  database;
+var backgroundIMG;
+var gameState = 0;
+var playerCount, form , player , game;
+var allPlayers
+
+function setup(){
+  database = firebase.database();
+  console.log(database);
+  createCanvas(400,400);
+
+  game = new Game()
+  game.getState()
+  game.start()
+  
+}
+
+function draw(){
+  background("white");
+  
+
+if(playerCount === 4){
+  game.update(1)
+}
+
+if(gameState === 1){
+  clear()
+
+  game.play()
+}
+    drawSprites();
+  
+}
+
